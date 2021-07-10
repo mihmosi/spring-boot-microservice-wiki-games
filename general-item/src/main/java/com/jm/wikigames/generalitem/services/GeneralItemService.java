084d7a9b0511c40dcea7a7c704bc22a69208abdc
Alisher Ochilov <sherifq@rambler.ru>
2021-07-10 12:55:20 +0300

    1021 - Описать сервис general item service и сделать его имплементацию

--- general-item/src/main/java/com/jm/wikigames/generalitem/services/GeneralItemService.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/services/GeneralItemService.java
@@ -1,4 +1,15 @@
 package com.jm.wikigames.generalitem.services;
 
+import com.jm.wikigames.generalitem.model.GeneralItemGame;
+
+import java.util.List;
+
 public interface GeneralItemService {
+    Long create(GeneralItemGame item);
+
+    List<GeneralItemGame> getAllItems();
+
+    GeneralItemGame getByName(String name);
+
+    void delete(Long id);
 }
---
+++ general-item/src/main/java/com/jm/wikigames/generalitem/services/GeneralItemServiceImpl.java
@@ -0,0 +1,48 @@
+package com.jm.wikigames.generalitem.services;
+
+import com.jm.wikigames.generalitem.model.GeneralItemGame;
+import com.jm.wikigames.generalitem.repository.GeneralItemGameRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class GeneralItemServiceImpl implements GeneralItemService {
+
+    private final GeneralItemGameRepository generalItemGameRepository;
+
+    public GeneralItemServiceImpl(GeneralItemGameRepository generalItemGameRepository) {
+        this.generalItemGameRepository = generalItemGameRepository;
+    }
+
+    @Override
+    @Transactional
+    public Long create(GeneralItemGame item) {
+        generalItemGameRepository.save(item);
+        return item.getId();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<GeneralItemGame> getAllItems() {
+        return generalItemGameRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public GeneralItemGame getByName(String name) {
+        GeneralItemGame item =  generalItemGameRepository.findByName(name);
+        if(item == null) {
+            throw new EntityNotFoundException();
+        }
+        return item;
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        generalItemGameRepository.deleteById(id);
+    }
+}
