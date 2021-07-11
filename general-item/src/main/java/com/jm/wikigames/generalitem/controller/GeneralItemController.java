02f6408f39232d6877ec6a6273e2de0ddb961c41
Станислав <yastas593@mail.ru>
2021-07-11 09:14:26 +0300

    1041 - Описать контроллер

--- general-item/src/main/java/com/jm/wikigames/generalitem/controller/GeneralItemController.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/controller/GeneralItemController.java
@@ -24,21 +24,10 @@ public class GeneralItemController {
     @PostMapping("/items")
     public String addItem(@RequestBody GeneralItemGame generalItemGame){
         try {
-            generalItemService.addItem(generalItemGame);
-            return "Item " + generalItemGame.getItemName() + " added successfully";
+            generalItemService.create(generalItemGame);
+            return "Item " + generalItemGame.getName() + " added successfully";
         } catch (SQLIntegrityConstraintViolationException ex) {
-            return "Item " + generalItemGame.getItemName() + " not add, item exists";
-        }
-    }
-
-    @PutMapping("/items")
-    public String updateItem(@RequestBody GeneralItemGame generalItemGame){
-        try {
-            GeneralItemGame generalItemGame = generalItemService.findItemById(generalItemGame.GetId());
-            generalItemService.updateItem(generalItemGame);
-            return "Item " + generalItemGame.getItemName() + " update successfully";
-        } catch (EntityNotFoundException ex){
-            return "Item witch id: " + generalItemGame.toString() + " not found";
+            return "Item " + generalItemGame.getName() + " not add, item exists";
         }
     }
 
@@ -48,26 +37,57 @@ public class GeneralItemController {
         return allItems;
     }
 
-    @GetMapping("/items/{id}")
-    public GeneralItemGame findItemById(@PathVariable long id){
+    @GetMapping("/items/{name}")
+    public GeneralItemGame findItemByName(@PathVariable String name){
         try{
-            GeneralItemGame generalItemGame = generalItemService.findItemById(id);
+            GeneralItemGame generalItemGame = generalItemService.findItemByName(name);
             return generalItemGame;
         } catch (EntityNotFoundException ex){
             return null;
         }
     }
 
-    @DeleteMapping("/items/{id}")
-    public String deleteItemById(@PathVariable long id){
+    @DeleteMapping("/items/{name}")
+    public String deleteItemByName(@PathVariable String name){
         try {
-            GeneralItemGame generalItemGame = generalItemService.findItemById(id);
-            generalItemService.deleteItem(generalItemGame);
-            return "Item " + generalItemGame.getItemName() + " delete successfully";
+            GeneralItemGame generalItemGame = generalItemService.findItemByName(name);
+            generalItemService.delete(generalItemGame);
+            return "Item " + generalItemGame.getName() + " delete successfully";
         } catch (EntityNotFoundException ex){
-            return "Item witch id: " + id + "not found";
+            return "Item witch name: " + name + "not found";
         }
     }
 
+//    @PutMapping("/items")
+//    public String updateItem(@RequestBody GeneralItemGame generalItemGame){
+//        try {
+//            GeneralItemGame generalItemGame = generalItemService.findItemById(generalItemGame.GetId());
+//            generalItemService.updateItem(generalItemGame);
+//            return "Item " + generalItemGame.getItemName() + " update successfully";
+//        } catch (EntityNotFoundException ex){
+//            return "Item witch id: " + generalItemGame.toString() + " not found";
+//        }
+//    }
+
+//    @DeleteMapping("/items/{id}")
+//    public String deleteItemById(@PathVariable long id){
+//        try {
+//            GeneralItemGame generalItemGame = generalItemService.findItemById(id);
+//            generalItemService.deleteItem(generalItemGame);
+//            return "Item " + generalItemGame.getItemName() + " delete successfully";
+//        } catch (EntityNotFoundException ex){
+//            return "Item witch id: " + id + "not found";
+//        }
+//    }
+
+//    @GetMapping("/items/{id}")
+//    public GeneralItemGame findItemById(@PathVariable long id){
+//        try{
+//            GeneralItemGame generalItemGame = generalItemService.findItemById(id);
+//            return generalItemGame;
+//        } catch (EntityNotFoundException ex){
+//            return null;
+//        }
+//    }
 
 }
