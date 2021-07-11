8e7ed2f52dd13e2085efcf84b0242fe366df52b2
Username-Karina <karina_rybalchenko@mail.ru>
2021-07-11 21:15:09 +0200

    1001 - Описать структуру модели general item

--- general-item/src/main/java/com/jm/wikigames/generalitem/model/GeneralItemGame.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/model/GeneralItemGame.java
@@ -1,5 +1,32 @@
 package com.jm.wikigames.generalitem.model;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id","name"})
 public class GeneralItemGame {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String name;
+    @NonNull
+    private Date dateOfCreation;
+    @NonNull
+    private String genre;
+    @NonNull
+    private String heroes;
+    @NonNull
+    private String villains;
 }
