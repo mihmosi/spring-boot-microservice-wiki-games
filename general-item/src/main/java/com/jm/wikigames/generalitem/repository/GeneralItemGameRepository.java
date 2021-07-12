20d5d503c6b854f05b6f2930aecc981a2feda549
Mitrxen <Mitrxen@Mitrxen>
2021-07-12 14:08:46 +0500

    1011 - Настроить репозиторий под JPA

--- general-item/src/main/java/com/jm/wikigames/generalitem/repository/GeneralItemGameRepository.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/repository/GeneralItemGameRepository.java
@@ -1,4 +1,8 @@
 package com.jm.wikigames.generalitem.repository;
 
-public class GeneralItemGameRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jm.wikigames.generalitem.model.GeneralItemGame;
+
+public interface GeneralItemGameRepository extends JpaRepository<GeneralItemGame, Long>{
 }
