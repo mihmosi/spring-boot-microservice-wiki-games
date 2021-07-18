dc625ef7c49bf34736e4d316c63645952faed832
Sherifq <43904737+Sherifq@users.noreply.github.com>
2021-07-18 16:58:40 +0300

    Revert "1021 - Описать сервис general item service и сделать его имплементацию"

--- general-item/src/main/java/com/jm/wikigames/generalitem/services/GeneralItemService.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/services/GeneralItemService.java
@@ -1,17 +1,4 @@
 package com.jm.wikigames.generalitem.services;
 
-import com.jm.wikigames.generalitem.model.GeneralItemGame;
-
-import java.util.List;
-
 public interface GeneralItemService {
-    GeneralItemGame create(GeneralItemGame item);
-
-    List<GeneralItemGame> getAllItems();
-
-    GeneralItemGame getByName(String name);
-
-    GeneralItemGame getById(Long id);
-
-    void delete(Long id);
 }
