52659b1dddd1cc82f8f8b2272d6a13606f5a8ea8
Alisher Ochilov <sherifq@rambler.ru>
2021-07-22 10:33:43 -0400

    WG-DEV-GM-1023 Доработка в GeneralItemGameRepository

--- general-item/src/main/java/com/jm/wikigames/generalitem/repository/GeneralItemGameRepository.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/repository/GeneralItemGameRepository.java
@@ -1,11 +1,12 @@
 package com.jm.wikigames.generalitem.repository;
 
-import java.util.List;
-
+import com.jm.wikigames.generalitem.model.GeneralItemGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.jm.wikigames.generalitem.model.GeneralItemGame;
+import java.util.Optional;
 
-public interface GeneralItemGameRepository extends JpaRepository<GeneralItemGame, Long>{
-	public List<GeneralItemGame> findByName(String name);
+public interface GeneralItemGameRepository extends JpaRepository<GeneralItemGame, Long> {
+
+    Optional<GeneralItemGame> findByName(String name);
+
 }
