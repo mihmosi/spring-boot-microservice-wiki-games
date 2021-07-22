7b95e9ceb18df07b9fd87df66abade41bd73e895
Alisher Ochilov <sherifq@rambler.ru>
2021-07-22 11:06:56 -0400

    WG-DEV-GM-1023 we dont use Optional

--- general-item/src/main/java/com/jm/wikigames/generalitem/repository/GeneralItemGameRepository.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/repository/GeneralItemGameRepository.java
@@ -3,10 +3,9 @@ package com.jm.wikigames.generalitem.repository;
 import com.jm.wikigames.generalitem.model.GeneralItemGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface GeneralItemGameRepository extends JpaRepository<GeneralItemGame, Long> {
 
-    Optional<GeneralItemGame> findByName(String name);
+    GeneralItemGame findByName(String name);
 
 }
