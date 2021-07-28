e451511560fc0115462a53da4b0629404b4b9cbe
Egor Trifonov <trifonov-egor@mail.ru>
2021-07-28 19:33:52 +0300

    1032- Создать класс Outside
    Удалено поле Id

--- general-item/src/main/java/com/jm/wikigames/generalitem/model/Outside.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/model/Outside.java
@@ -11,15 +11,11 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.util.Date;
 
-@Entity
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Outside {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     @Enumerated(EnumType.STRING)
     private Platform platform;
