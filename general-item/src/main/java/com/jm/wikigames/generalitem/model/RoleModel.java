6a708bf7aa3c5793e9e9008736550e267346304c
stas593 <yastas593@mail.ru>
2021-07-28 07:29:25 +0300

    1045 - добавить UserRepository и RoleModel для пользователей

---
+++ general-item/src/main/java/com/jm/wikigames/generalitem/model/RoleModel.java
@@ -0,0 +1,34 @@
+package com.jm.wikigames.generalitem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleModel implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nameRole;
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH}, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role"
+            , joinColumns = @JoinColumn(name = "role_id")
+            , inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<UserModel> users;
+
+    @Override
+    public String getAuthority() {
+        return this.nameRole;
+    }
+}
