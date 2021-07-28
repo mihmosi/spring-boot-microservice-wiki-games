797b218e306a6f6cdcba086b02b1776cc1e2a380
Username-Karina <karina_rybalchenko@mail.ru>
2021-07-28 20:39:56 +0300

    1004 - Поправки в Inside + изменение Character + добавлен CharacterStatus

--- general-item/src/main/java/com/jm/wikigames/generalitem/model/Inside.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/model/Inside.java
@@ -1,43 +1,25 @@
 package com.jm.wikigames.generalitem.model;
 
-import com.jm.wikigames.generalitem.model.characters.Antagonist;
-import com.jm.wikigames.generalitem.model.characters.MinorCharacter;
-import com.jm.wikigames.generalitem.model.characters.Protagonist;
+
 import com.jm.wikigames.generalitem.model.enums.Genre;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
 
-@Entity
+
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
-@Table(name = "inside")
 public class Inside {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private List<Character> characters;
 
-    @Column(name = "protagonists")
-    @OneToMany(fetch = FetchType.LAZY)
-    private List<Protagonist> protagonists;
-
-    @Column(name = "antagonist")
-    @OneToMany(fetch = FetchType.LAZY)
-    private List<Antagonist> antagonists;
-
-    @Column(name = "minorСharacter")
-    @OneToMany(fetch = FetchType.LAZY)
-    private List<MinorCharacter> minorСharacters;
-
-    @Column(name = "lore")
     private String lore;
 
-    @Column(name = "originalSourse")
     private String originalSourse;
 
-    @Column(name = "genre")
     @Enumerated(EnumType.STRING)
     private Genre genre;
 }
+
--- general-item/src/main/java/com/jm/wikigames/generalitem/model/characters/Character.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/model/characters/Character.java
@@ -1,22 +1,25 @@
 package com.jm.wikigames.generalitem.model.characters;
 
+import com.jm.wikigames.generalitem.model.enums.CharacterStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
-@MappedSuperclass
-public abstract class Character {
+@AllArgsConstructor
+@Entity
+@Table(name = "character")
+public class Character {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Enumerated(EnumType.STRING)
+    private CharacterStatus status;
     private String sex;
     private String race;
     private String faction;
