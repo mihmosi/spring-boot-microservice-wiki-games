ac17ea3ad2849b2d3a270c5039003754f964f2c2
rojobae <trifonov-egor@mail.ru>
2021-07-30 20:12:04 +0300

    1044 - Написать тест для конвертера Status'ов

---
+++ general-item/src/test/java/com/jm/wikigames/generalitem/converter/StatusEnumToStringConverterTest.java
@@ -0,0 +1,25 @@
+package com.jm.wikigames.generalitem.converter;
+
+import com.jm.wikigames.generalitem.model.enums.Status;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatusEnumToStringConverterTest {
+
+    @Test
+    public void enumToString_test() {
+        StatusEnumToStringConverter statusEnumToStringConverter = new StatusEnumToStringConverter();
+
+        Assertions.assertEquals(statusEnumToStringConverter.convertToDatabaseColumn(Status.RELEASE), "Release");
+        Assertions.assertEquals(statusEnumToStringConverter.convertToDatabaseColumn(Status.PRODUCTION), "Production");
+    }
+
+    @Test
+    public void  stringToEnum_test() {
+        StatusEnumToStringConverter statusEnumToStringConverter = new StatusEnumToStringConverter();
+
+        Assertions.assertEquals(statusEnumToStringConverter.convertToEntityAttribute("Release"), Status.RELEASE);
+        Assertions.assertEquals(statusEnumToStringConverter.convertToEntityAttribute("Production"), Status.PRODUCTION);
+    }
+
+}
