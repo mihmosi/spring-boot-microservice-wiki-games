0a615244b2cb6c12fda519d589e1d068bbc5136b
stas593 <yastas593@mail.ru>
2021-08-02 11:39:17 +0300

    WG-DEV-GM-1045 перенос в user-service

--- user-service/src/main/java/com/jm/wikigames/userservice/model/RoleModel.java
+++ user-service/src/main/java/com/jm/wikigames/userservice/model/RoleModel.java
@@ -1,4 +1,4 @@
-package com.jm.wikigames.generalitem.model;
+package com.jm.wikigames.userservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- user-service/src/main/java/com/jm/wikigames/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/jm/wikigames/userservice/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.jm.wikigames.generalitem.repository;
+package com.jm.wikigames.userservice.repository;
 
-import com.jm.wikigames.generalitem.model.UserModel;
+import com.jm.wikigames.userservice.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
