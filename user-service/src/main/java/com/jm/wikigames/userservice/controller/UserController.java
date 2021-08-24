2d413668e314f195a2e8d8420429e85b7f6aaa3b
pschenizzza <guitar57@yandex.ru>
2021-08-24 18:44:29 +0700

    WG-DEV-GM-1061 Поправил UserModel, сделал контроллер вывода, секьюрити конфиг с багом изначально

---
+++ user-service/src/main/java/com/jm/wikigames/userservice/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.jm.wikigames.userservice.controller;
+
+import com.jm.wikigames.userservice.model.UserModel;
+import com.jm.wikigames.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/show")
+    public List <UserModel> getAllUsers(){
+        return userService.getAllUsers ();
+    }
+
+}
--- user-service/src/main/java/com/jm/wikigames/userservice/model/UserModel.java
+++ user-service/src/main/java/com/jm/wikigames/userservice/model/UserModel.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Set;
 
 
 @Entity
@@ -28,12 +29,15 @@ public class UserModel implements UserDetails {
 	private String name;
 	private String password;
 	private String email;
-	private RoleModel role;
+	@ManyToMany
+	private Set<RoleModel> role;
+
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return this.roles;
+		return this.role;
 	}
+
 
 	@Override
 	public String getUsername() {
