266ff37eaccaefdc91e8732f50ca51f5a1c9b28d
Alexander Fedotov <a.fedotov.dev@gmail.com>
2021-08-25 23:12:03 +0300

    WG-DEV-GM-1001 - Сделать парсер сайта - рефакторинг в процессе

--- general-item/src/main/java/com/jm/wikigames/generalitem/parsers/GenreParser.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/parsers/GenreParser.java
@@ -3,5 +3,6 @@ package com.jm.wikigames.generalitem.parsers;
 import java.util.Map;
 
 public interface GenreParser {
-    public Map<String, String> parseGenreMap(String url);
+//    public Map<String, String> parseGenreMap(String url);
+public Map<String, String> parseGenreMap();
 }
