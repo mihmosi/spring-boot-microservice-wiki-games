f248fb19ebaf03280a7bef6f36f93d0d40918ae1
Alexander Fedotov <a.fedotov.dev@gmail.com>
2021-08-26 16:56:32 +0300

    WG-DEV-GM-1001 - выделил методы, заменил циклы на стримы

---
+++ general-item/src/main/java/com/jm/wikigames/generalitem/parsers/impl/IdTechGenreParser.java
@@ -0,0 +1,121 @@
+package com.jm.wikigames.generalitem.parsers.impl;
+
+import lombok.Data;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import com.jm.wikigames.generalitem.parsers.GenreParser;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Data
+public class IdTechGenreParser implements GenreParser {
+    private final String userAgent = "Chrome/92.0.4515.159";
+    private final String referrer = "http://www.google.com";
+    private Elements parsedPage;
+    private Elements parsedKeyNames;
+    private List<Integer> keyIndexList;
+    private List<String> parsedValues;
+    private Map<String, String> genreDescriptionMap;
+
+    @Override
+    public Map<String, String> parseGenreMap(String url, String cssQuery, String excludeCssQuery, String keysCssQuery) {
+        getParsedPage(url, cssQuery, excludeCssQuery);
+        getParsedKeyNames(keysCssQuery);
+        getKeyIndexList(keysCssQuery);
+        getValues(keyIndexList);
+
+        if (genreDescriptionMap == null) {
+            genreDescriptionMap = new HashMap<>();
+
+            for (int i = 0; i < parsedKeyNames.size(); i++) {
+                genreDescriptionMap.put(parsedKeyNames.get(i).text(), parsedValues.get(i));
+            }
+        }
+        return genreDescriptionMap;
+    }
+
+
+    @Override
+    public Map<String, String> updateGenreMap(String url, String cssQuery, String excludeCssQuery, String keysCssQuery) {
+        resetValues();
+        return parseGenreMap(url, cssQuery, excludeCssQuery, keysCssQuery);
+    }
+
+
+    private void resetValues() {
+        parsedPage = null;
+        parsedKeyNames = null;
+        keyIndexList = null;
+        parsedValues = null;
+    }
+
+
+    private Elements getParsedPage(String url, String cssQuery, String excludeCssQuery) {
+        if (parsedPage == null) {
+            try {
+                parsedPage = Jsoup
+                        .connect(url)
+                        .userAgent(userAgent)
+                        .referrer(referrer)
+                        .get()
+                        .select(cssQuery)
+                        .not(excludeCssQuery);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return parsedPage;
+    }
+
+    private Elements getParsedKeyNames(String keysCssQuery) {
+        if (parsedKeyNames == null) {
+            parsedKeyNames = parsedPage.select(keysCssQuery);
+
+            parsedKeyNames = new Elements(parsedKeyNames
+                    .stream()
+                    .filter(e -> (parsedPage.get(parsedPage.indexOf(e)).text().length() > 2))
+                    .collect(Collectors.toList()));
+        }
+        return parsedKeyNames;
+    }
+
+
+    private List<Integer> getKeyIndexList(String keysCssQuery) {
+        if (keyIndexList == null) {
+            keyIndexList = parsedKeyNames
+                    .stream()
+                    .map(e -> parsedPage.indexOf(e))
+                    .collect(Collectors.toList());
+        }
+        return keyIndexList;
+    }
+
+
+    private List<String> getValues(List<Integer> keyIndexList) {
+        if (parsedValues == null) {
+            parsedValues = new ArrayList<>();
+
+            for (int i = 0; i < (keyIndexList.size() - 1); i++) {
+                String valueDescription = parsedPage
+                        .subList((keyIndexList.get(i) + 1), (keyIndexList.get(i + 1)))
+                        .stream()
+                        .map(e -> {
+                            StringBuilder sb = new StringBuilder();
+                            return sb.append(e.text());
+                        })
+                        .reduce(new StringBuilder(), StringBuilder::append).toString();
+                parsedValues.add(valueDescription);
+            }
+            parsedValues.add(parsedPage
+                    .subList((keyIndexList.get(keyIndexList.size() - 1) + 1), parsedPage.size())
+                    .stream()
+                    .map(e -> {
+                        StringBuilder sb = new StringBuilder();
+                        return sb.append(e.text());
+                    })
+                    .reduce(new StringBuilder(), StringBuilder::append).toString());
+        }
+        return parsedValues;
+    }
+}
