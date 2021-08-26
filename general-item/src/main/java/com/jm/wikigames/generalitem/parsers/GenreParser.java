d9a6ecadc154c6e6df432a82fdd0e15f6f8275ab
Alexander Fedotov <a.fedotov.dev@gmail.com>
2021-08-26 16:56:32 +0300

    WG-DEV-GM-1001 - выделил методы, заменил циклы на стримы. Реализация не выделяет поджанры в отдельные ключи, только основные жанры.

--- general-item/src/main/java/com/jm/wikigames/generalitem/parsers/GenreParser.java
+++ general-item/src/main/java/com/jm/wikigames/generalitem/parsers/GenreParser.java
@@ -3,6 +3,8 @@ package com.jm.wikigames.generalitem.parsers;
 import java.util.Map;
 
 public interface GenreParser {
-//    public Map<String, String> parseGenreMap(String url);
-public Map<String, String> parseGenreMap();
+
+    public Map<String, String> parseGenreMap(String url, String cssQuery, String excludeCssQuery, String keysCssQuery);
+
+    public Map<String, String> updateGenreMap(String url, String cssQuery, String excludeCssQuery, String keysCssQuery);
 }
