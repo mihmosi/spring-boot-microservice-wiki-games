a34c1906b98813b9e3d614ae5c474e2440611fe3
svetlanaaseeva <antaseev@yandex.ru>
2021-09-02 12:02:08 +0300

    WG-DEV-GM-1021 - Исправлен конфликт. Изменил URL

--- game-item/src/main/java/com/jm/wikigames/gameitem/repository/GameItemRepository.java
+++ game-item/src/main/java/com/jm/wikigames/gameitem/repository/GameItemRepository.java
@@ -2,8 +2,9 @@ package com.jm.wikigames.gameitem.repository;
 
 import com.jm.wikigames.gameitem.model.GameItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface GameItemRepository extends JpaRepository<GameItem, Long> {
 
     GameItem findByName(String name);
