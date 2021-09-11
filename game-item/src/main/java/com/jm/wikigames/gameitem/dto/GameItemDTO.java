eb0b6782448c98e215bfd0b48df20d3a4d1e27fb
in3des <62700347+in3des@users.noreply.github.com>
2021-09-11 11:23:44 +0400

    WG-DEV-GM-2043 - Ввести DTO для game-item

--- game-item/src/main/java/com/jm/wikigames/gameitem/dto/GameItemDTO.java
+++ game-item/src/main/java/com/jm/wikigames/gameitem/dto/GameItemDTO.java
@@ -1,4 +1,21 @@
 package com.jm.wikigames.gameitem.dto;
 
+import com.jm.wikigames.gameitem.model.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GameItemDTO {
+
+    private Long id;
+    private String name;
+    private String dateOfCreation;
+    private String genre;
+    private String heroes;
+    private String villains;
+    private Status status;
+
 }
