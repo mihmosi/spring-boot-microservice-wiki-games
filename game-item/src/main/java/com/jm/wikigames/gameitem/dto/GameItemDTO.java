8eec653d81743bf583c7786a680b65dbffa60969
in3des <62700347+in3des@users.noreply.github.com>
2021-09-11 11:44:26 +0400

    WG-DEV-GM-2043 - добавил дополнительные поля и комментарии

--- game-item/src/main/java/com/jm/wikigames/gameitem/dto/GameItemDTO.java
+++ game-item/src/main/java/com/jm/wikigames/gameitem/dto/GameItemDTO.java
@@ -10,12 +10,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class GameItemDTO {
 
-    private Long id;
+    private Long id; //возможно лишнее, нет в описании
     private String name;
-    private String dateOfCreation;
+    private String lore; //new
+    private String originalSource; //new
     private String genre;
-    private String heroes;
-    private String villains;
+    private String platform; //new
+    private String dateOfCreation;
+    private String distributor; //new
+    private String outsideGenre; //new - экшн, стратегия, РПГ, аркада...
+    private String gameUniversity; //new
+    private String systemRequirements; //new
+    private String gameMode; //new - режим игры ( одиночная, кооператив...)
+    private String heroes; //возможно лишнее, нет в описании
+    private String villains; //возможно лишнее, нет в описании
     private Status status;
+    private String link; //new
 
 }
