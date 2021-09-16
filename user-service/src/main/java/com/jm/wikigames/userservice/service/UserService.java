e7287e5c0b970acf19bb3d9d77c1a293199f81d4
svetlanaaseeva <antaseev@yandex.ru>
2021-09-16 12:01:25 +0300

    WG-DEV-GM-2025- Изменить метод create в user-service

--- user-service/src/main/java/com/jm/wikigames/userservice/service/UserService.java
+++ user-service/src/main/java/com/jm/wikigames/userservice/service/UserService.java
@@ -1,5 +1,6 @@
 package com.jm.wikigames.userservice.service;
 
+import com.jm.wikigames.userservice.dto.UserCreatingDTO;
 import com.jm.wikigames.userservice.model.UserModel;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface UserService {
 
-    UserModel create(UserModel userModel);
+    UserModel create(UserCreatingDTO userCreatingDto);
 
     List<UserModel> getAllUsers();
 
