65a506debcb1674d64120f32302ee2de70ccab1f
Eugene <79109896+Chester-Cheetah@users.noreply.github.com>
2021-09-20 16:19:21 +0300

    WG-DEV-GM-2053- реализовать метод writeToXML

--- writer-service/src/main/java/com/jm/wikigames/writerservice/service/impl/WriterImpl.java
+++ writer-service/src/main/java/com/jm/wikigames/writerservice/service/impl/WriterImpl.java
@@ -3,9 +3,12 @@ package com.jm.wikigames.writerservice.service.impl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jm.wikigames.writerservice.dto.GameItemDTO;
 import com.jm.wikigames.writerservice.service.Writer;
-
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.FileSystems;
+import java.util.Arrays;
 
 public class WriterImpl implements Writer {
 
@@ -23,6 +26,16 @@ public class WriterImpl implements Writer {
 
     @Override
     public String writeToXML(GameItemDTO gameItemDTO) {
-        return null;
+        String filePath = String.join(FileSystems.getDefault().getSeparator(), Arrays.asList(".", "writer-service", "src", "main", "resources", "gameItemDTO.xml"));
+        File file = new File(filePath);
+        String xml = null;
+        try (java.io.Writer writer = new PrintWriter(file)){
+            file.createNewFile();
+            xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + new XmlMapper().writeValueAsString(gameItemDTO);
+            writer.write(xml);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return xml;
     }
 }
